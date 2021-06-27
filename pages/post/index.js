15a95e203f174cde6a80613901e275bb7f064a68
mubin86 <mubinur@ssd-tech.io>
2021-06-28 01:36:55 +0600

    updated structure

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,4 +1,5 @@
 import { Fragment } from 'react'
+import Link from 'next/link'
 
 const PostPage = () => {
     return (
@@ -6,6 +7,19 @@ const PostPage = () => {
         <h1>
             All the Posts from the students
         </h1>
+        <br/>
+        <ul>
+            <li>
+                <Link href='/post/7'> 
+                    first post
+                </Link>
+            </li>
+            <li>
+                <Link href='/post/8'> 
+                    Second post
+                </Link>
+            </li>
+        </ul>
         </Fragment>
         
     );
