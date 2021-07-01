b5ed672823cd7429d41a03efd6b8699e414b9fc5
mubin86 <mubinur@ssd-tech.io>
2021-07-02 01:43:16 +0600

    navbar updated

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function MainNavigation() {
     return (
-        <div className="mb-7">
-            hello people this is the Navbar
+        <div className="mb-7 block bg-blue-400 w-full">
+            {/* hello people this is the Navbar */}
+            <header>
+                <nav>
+                    <ul className="flex justify-evenly p-4 text-3xl">
+                        <li>
+                            <Link href='/'>All Posts</Link>
+                        </li>
+                        <li> 
+                            <Link href='/post/new-post'>Create New Post</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         </div>
     )
 }
