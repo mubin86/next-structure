f92942d084b720d58822748cb0fefbdbc400f3e3
mubin86 <rahman.mmubinur@gmail.com>
2021-07-03 02:15:09 +0600

    Layout will be integrated accordingly to conditionally in app.js bcz of Login page

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import Layout from '../components/layout/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const n=1;
+
   return (
+    n==1 ?
     <Layout>
         <Component {...pageProps} />
-    </Layout>
+    </Layout>: <Component {...pageProps} />
 
   )
   
