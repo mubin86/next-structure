fa9e3fbd69452a3a5af75d1fdf40fda3af39c3cb
mubin86 <rahman.mmubinur@gmail.com>
2021-07-03 06:51:36 +0600

    inserted post in mongodb

--- pages/post/new-post/index.js
+++ pages/post/new-post/index.js
@@ -1,11 +1,24 @@
 import { Fragment } from 'react'
+import axios from 'axios';
 import NewPost from '../../../components/post/NewPost'
 
 
 export default function NewPostPage() {
 
-    const newPostHandler = (postData) => {
+    const newPostHandler = async (postData) => {
             console.log("postData is",postData);
+
+            const response = await axios({
+                method: 'POST',
+                url: '/api/new-post',
+                data: JSON.stringify(postData),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+
+            const data = await response.json();
+            console.log("response data is". data);
     }
 
 
