fb9871c8c05cdcfe93f72c3483a8ad37b946b823
mubin86 <rahman.mmubinur@gmail.com>
2021-07-04 00:53:52 +0600

    fixed small thing in UI

--- components/post/PostItem.js
+++ components/post/PostItem.js
@@ -9,13 +9,13 @@ export default function PostItem({postId, title, description}) {
 
     return (
         <div className="flex justify-center">
-        <div className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+        <div className="max-w-md py-4 px-8 bg-gray-200 shadow-lg rounded-lg my-12">
             <div className="flex justify-center md:justify-end -mt-16">
                 <img className="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" />
             </div>
             <div>
                 <h2 className="text-gray-800 text-3xl font-semibold">{title} and _id is {postId}</h2>
-                <p className="mt-2 text-gray-600">{description}</p>
+                {/* <p className="mt-2 text-gray-600">{description}</p> */}
             </div>
             <div className="flex justify-end mt-4">
                 <a href="#" className="text-xl font-medium text-indigo-500">John Doe</a>
