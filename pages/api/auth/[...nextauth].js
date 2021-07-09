f32977f7b5d1fc7369e475b6e6ffdb554a45bfec
mubin86 <rahman.mmubinur@gmail.com>
2021-07-10 03:02:28 +0600

    integrated successfully twilio sendgrid email signin

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,20 +7,19 @@ const options ={
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
         }),
-        // Providers.Email({
-        //     server: {
-        //         host: "",
-        //         port: "",
-        //         auth: {
-        //             user: "",
-        //             pass: ""
-        //         }
-        //     },
-        //     from: "",
-        // })
-
+        Providers.Email({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: process.env.EMAIL_SERVER_PORT,
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD
+                }
+            },
+            from: process.env.EMAIL_FROM
+        })
     ],
-    // database: process.env.DATABASE_URL
+    database: process.env.DATABASE_URL
 }
 
 export default (req, res) => NextAuth(req, res, options);
