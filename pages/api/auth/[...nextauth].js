3772fc646579572a0156e3cf4f81fd869b404a09
mubin86 <rahman.mmubinur@gmail.com>
2021-07-09 17:02:53 +0600

    integrating next-auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+const options ={
+    providers: [
+        Providers.GitHub({
+            clientId: "",
+            clientSecret: "" 
+        }),
+        Providers.Email({
+            server: {
+                host: "",
+                port: "",
+                auth: {
+                    user: "",
+                    pass: ""
+                }
+            },
+            from: "",
+        })
+    ]
+}
